ee883c8f0b89fdbd14d663de35869baa3278cb10
Muthazhagu (Muthu) Palanisamy <muthazhagu@gmail.com>
2014-07-31 22:01:08 -0400

    Added the findos() method.

--- tabulatepollutantdata.py
+++ tabulatepollutantdata.py
@@ -1,7 +1,19 @@
 from writefinalfile import writefinalfile
+import os
+import sys
+
+def findos():
+    if os.name == 'posix':
+        root_dir = '/'
+    elif os.name == 'nt':
+        root_dir = 'C:\\'
+    else:
+        print('Unknown OS.')
+        sys.exit()
 
 
 def main():
+    findos()
     finalresultfile = open('/Users/muthu/Downloads/keerthi/eis_report/finalresult.csv', 'w')
     writefinalfile('/Users/muthu/Downloads/keerthi/eis_report/results.csv', finalresultfile)
 
