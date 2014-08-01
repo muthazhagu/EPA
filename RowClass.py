301a9cc9cebd6e4f06a8487941083ae1ad23ebb4
Muthazhagu (Muthu) Palanisamy <muthazhagu@gmail.com>
2014-07-31 20:27:00 -0400

    Created a simpler RowClass. This class has simpler attributes. No more popper() type code when using this class.

--- RowClass.py
+++ RowClass.py
@@ -4,31 +4,90 @@ class Row:
         self.state = ''
         self.efsi = ''
         self.fsn = ''
-        self.lead = []
-        self.co = []
-        self.nox = []
-        self.pmpri = []
-        self.so2 = []
-        self.voc = []
+        self._lead = 'no data'
+        self._co = 'no data'
+        self._nox = 'no data'
+        self._pmpri = 'no data'
+        self._so2 = 'no data'
+        self._voc = 'no data'
 
-    def longestlist(self):
-        length = [len(item) for item in [self.lead, self.co, self.nox, self.pmpri, self.so2, self.voc]]
-        return max(length)
+    @property
+    def lead(self):
+        return self._lead
 
-    def popper(self):
-        poppeditems = [item.pop(0) if item else '' for item in
-                       [self.lead, self.co, self.nox, self.pmpri, self.so2, self.voc]]
-        return poppeditems
+    @lead.setter
+    def lead(self, value):
+        if self._lead == 'no data':
+            self._lead = value
+        elif self._lead < value:
+            self._lead = value
+            
+    @property
+    def co(self):
+        return self._co
+
+    @co.setter
+    def co(self, value):
+        if self._co == 'no data':
+            self._co = value
+        elif self._co < value:
+            self._co = value
+            
+    @property
+    def nox(self):
+        return self._nox
+
+    @nox.setter
+    def nox(self, value):
+        if self._nox == 'no data':
+            self._nox = value
+        elif self._nox < value:
+            self._nox = value
+            
+    @property
+    def pmpri(self):
+        return self._pmpri
+
+    @pmpri.setter
+    def pmpri(self, value):
+        if self._pmpri == 'no data':
+            self._pmpri = value
+        elif self._pmpri < value:
+            self._pmpri = value
+            
+    @property
+    def so2(self):
+        return self._so2
+
+    @so2.setter
+    def so2(self, value):
+        if self._so2 == 'no data':
+            self._so2 = value
+        elif self._so2 < value:
+            self._so2 = value
+            
+    @property
+    def voc(self):
+        return self._voc
+
+    @voc.setter
+    def voc(self, value):
+        if self._voc == 'no data':
+            self._voc = value
+        elif self._voc < value:
+            self._voc = value
+
+    def signature(self):
+        return ','.join([self.year, self.state, self.efsi, self.fsn])
 
     def __str__(self):
-        string = ''
-        for i in range(self.longestlist()):
-            string += self.year + ',' + self.state + ',' + self.efsi + ',' + self.fsn + ',' + ','.join(
-                self.popper()) + '\n'
-        return string
+        return ','.join([self.year, self.state, self.efsi, self.fsn, self.lead, self.co, self.nox, self.pmpri, self.so2,
+                         self.voc])
 
     def __eq__(self, other):
         return self.year == other.year and \
                self.state == other.state and \
                self.efsi == other.efsi and \
                self.fsn == other.fsn
+
+
---
+++ RowClass2.py
@@ -0,0 +1,34 @@
+class Row:
+    def __init__(self):
+        self.year = ''
+        self.state = ''
+        self.efsi = ''
+        self.fsn = ''
+        self.lead = []
+        self.co = []
+        self.nox = []
+        self.pmpri = []
+        self.so2 = []
+        self.voc = []
+
+    def longestlist(self):
+        length = [len(item) for item in [self.lead, self.co, self.nox, self.pmpri, self.so2, self.voc]]
+        return max(length)
+
+    def popper(self):
+        poppeditems = [item.pop(0) if item else '' for item in
+                       [self.lead, self.co, self.nox, self.pmpri, self.so2, self.voc]]
+        return poppeditems
+
+    def __str__(self):
+        string = ''
+        for i in range(self.longestlist()):
+            string += self.year + ',' + self.state + ',' + self.efsi + ',' + self.fsn + ',' + ','.join(
+                self.popper()) + '\n'
+        return string
+
+    def __eq__(self, other):
+        return self.year == other.year and \
+               self.state == other.state and \
+               self.efsi == other.efsi and \
+               self.fsn == other.fsn
