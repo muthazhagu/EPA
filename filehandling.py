bbb3ea7411cdf4a331d60f3790f569ae774025e5
Muthazhagu (Muthu) Palanisamy <muthazhagu@gmail.com>
2014-08-02 22:28:03 -0400

    Removed print stmts.

--- filehandling.py
+++ filehandling.py
@@ -30,11 +30,9 @@ def checkworkingdir():
 def dirsetup():
     if checkworkingdir():
         workingdir = os.getcwd()  # must return the epa directory
-        print(os.listdir(workingdir))
         recreatedir(workingdir, 'log')
         recreatedir(workingdir, 'processed')
         recreatedir(workingdir, 'output')
-        print(os.listdir(workingdir))
         inputdir = os.path.join(workingdir, 'input')
         logdir = os.path.join(workingdir, 'log')
         processeddir = os.path.join(workingdir, 'processed')
@@ -49,7 +47,6 @@ def getiofiledata():
     inputdir, logdir, processeddir, outputdir = dirsetup()
     os.chdir(inputdir)
     workingdir = os.getcwd()  # in the input dir now
-    print(workingdir)
     filedata = [
         (os.path.join(workingdir, file), str(file[:4]), os.path.join(outputdir, os.path.splitext(file)[0] + '_output.csv'))
         for file in os.listdir(workingdir) if file.endswith('.csv')]
