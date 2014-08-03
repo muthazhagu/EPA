1a86e774192ae1e53b60bdbcaa0a20f2d8e0ca84
Muthazhagu (Muthu) Palanisamy <muthazhagu@gmail.com>
2014-08-02 22:10:22 -0400

    Clean-up, and multiple updates. This version iterates through a directory, and gives the absolute input, output paths, and the year of the file, based on what it is in the filename.

--- filehandling.py
+++ filehandling.py
@@ -4,7 +4,14 @@ import shutil
 import sys
 
 
-os.chdir('/Users/muthu/Downloads/keerthi/eis_report/epa')
+def findos():
+    if os.name == 'posix':
+        root_dir = '/'
+    elif os.name == 'nt':
+        root_dir = 'C:\\'
+    else:
+        print('Unknown OS.')
+        sys.exit()
 
 
 def recreatedir(workingdir, dirname):
@@ -16,29 +23,36 @@ def recreatedir(workingdir, dirname):
 
 
 def checkworkingdir():
-    workingdir = os.getcwd()
+    workingdir = os.getcwd()  # always run this program from within a directory named epa.
     return workingdir.endswith('epa')
 
 
 def dirsetup():
     if checkworkingdir():
-        workingdir = os.getcwd()
+        workingdir = os.getcwd()  # must return the epa directory
         print(os.listdir(workingdir))
         recreatedir(workingdir, 'log')
         recreatedir(workingdir, 'processed')
         recreatedir(workingdir, 'output')
         print(os.listdir(workingdir))
+        inputdir = os.path.join(workingdir, 'input')
+        logdir = os.path.join(workingdir, 'log')
+        processeddir = os.path.join(workingdir, 'processed')
+        outputdir = os.path.join(workingdir, 'output')
+        return inputdir, logdir, processeddir, outputdir
     else:
         print("Incorrect directory for data processing!")
         sys.exit()
 
-dirsetup()
-os.chdir('input')
-workingdir = os.getcwd()
-print(workingdir)
-filedata = [(os.path.join(workingdir, file), file[:4], os.path.splitext(file)[0]+'_output.csv')
-              for file in os.listdir(workingdir) if file.endswith('.csv')]
-for val in filedata:
-    inputfile, year, outputfile = val
-    print(inputfile, year, outputfile)
+
+def getiofiledata():
+    inputdir, logdir, processeddir, outputdir = dirsetup()
+    os.chdir(inputdir)
+    workingdir = os.getcwd()  # in the input dir now
+    print(workingdir)
+    filedata = [
+        (os.path.join(workingdir, file), str(file[:4]), os.path.join(outputdir, os.path.splitext(file)[0] + '_output.csv'))
+        for file in os.listdir(workingdir) if file.endswith('.csv')]
+    return filedata
+
 
