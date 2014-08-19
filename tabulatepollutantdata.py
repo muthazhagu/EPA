fb213dbf52affb19f32ff6c5e12c28302e43d6f6
Muthazhagu (Muthu) Palanisamy <muthazhagu@gmail.com>
2014-08-18 21:03:46 -0400

    Added code for calling the vrfiles() method.

--- tabulatepollutantdata.py
+++ tabulatepollutantdata.py
@@ -3,6 +3,7 @@ from filehandling import findos
 from filehandling import getiofiledata
 from filehandling import joinfiles
 from filehandling import sortfiles
+from filehandling import vrfiles
 from datetime import datetime
 
 
@@ -22,6 +23,9 @@ def main():
     # print("Sorting files.")
     # sortfiles()
     # print("Finished sorting files.")
+    print("Writing violation records.")
+    vrfiles()
+    print("Finished writing violation records.")
     endtime = datetime.today()
     print("Process took: {} seconds".format((endtime - starttime).total_seconds()))
 
