7776f08ebe1672861727d2c63239979191254bd8
Muthazhagu (Muthu) Palanisamy <muthazhagu@gmail.com>
2014-08-19 22:37:43 -0400

    Updated pollutant name, and fixed mean data calculation.

--- ViolationRecordProcessing.py
+++ ViolationRecordProcessing.py
@@ -30,11 +30,11 @@ def createdict(aFile):
             if key not in d:
                 d[key] = {}
                 value = {}
-                value[row['year']] = [row['7439921'], row['CO'], row['NOX'], row['PM-PRI'], row['SO2'], row['VOC']]
+                value[row['year']] = [row['7439921'], row['CO'], row['NOX'], row['PM10-PRI'], row['SO2'], row['VOC']]
                 d[key].update(value)
             else:
                 value = {}
-                value[row['year']] = [row['7439921'], row['CO'], row['NOX'], row['PM-PRI'], row['SO2'], row['VOC']]
+                value[row['year']] = [row['7439921'], row['CO'], row['NOX'], row['PM10-PRI'], row['SO2'], row['VOC']]
                 d[key].update(value)
 
         return d
@@ -43,17 +43,17 @@ def createdict(aFile):
 def create_vr_file(aFile):
     d = createdict(aFile)
 
-    newFile = 'violationrecords_' + aFile
+    newFile = 'followup_details_' + aFile
     with open(newFile, 'w') as f:
         f.writelines(','.join(
             ['eis_facility_site_id', 'facility_site_name', 'addr_state_cd', 'year',
              '7439921_latest', '7439921_mean',
              'CO_latest', 'CO_mean',
              'NOX_latest', 'NOX_mean',
-             'PM-PRI_latest', 'PM-PRI_mean',
+             'PM10-PRI_latest', 'PM10-PRI_mean',
              'SO2_latest', 'SO2_mean',
              'VOC_latest', 'VOC_mean',
-             'Recommendation\n']))
+             'Follow-up recommended?\n']))
         for key in d.keys():
             allkeys = list(d[key].keys())
             allkeys.sort()
@@ -65,12 +65,18 @@ def create_vr_file(aFile):
             remvalues = list(zip(*[d[key][subkey] for subkey in remainingkeys]))
 
             if remvalues:
-                lead, co, no2, pmpri, so2, voc = remvalues
-                lead, co, no2, pmpri, so2, voc = list(lead), list(co), list(no2), list(pmpri), list(so2), list(voc)
-                lead, co, no2, pmpri, so2, voc = removenodata(lead), removenodata(co), removenodata(no2), removenodata(
+                lead, co, nox, pmpri, so2, voc = remvalues
+                lead, co, nox, pmpri, so2, voc = list(lead), list(co), list(nox), list(pmpri), list(so2), list(voc)
+                lead, co, nox, pmpri, so2, voc = removenodata(lead), removenodata(co), removenodata(nox), removenodata(
                     pmpri), removenodata(so2), removenodata(voc)
 
-                meandata = [findmean(lead), findmean(co), findmean(no2), findmean(pmpri), findmean(so2), findmean(voc)]
+
+                meandata = [findmean(lead) + Decimal('0.6') if not findmean(lead) == 'no data' else findmean(lead),
+                            findmean(co) + Decimal('100') if not findmean(co) == 'no data' else findmean(co),
+                            findmean(nox) + Decimal('40') if not findmean(nox) == 'no data' else findmean(nox),
+                            findmean(pmpri) + Decimal('15') if not findmean(pmpri) == 'no data' else findmean(pmpri),
+                            findmean(so2) + Decimal('40') if not findmean(so2) == 'no data' else findmean(so2),
+                            findmean(voc) + Decimal('40') if not findmean(voc) == 'no data' else findmean(voc)]
                 meandata = map(str, meandata)
 
             vr = ViolationRecord()
