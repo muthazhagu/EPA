2e6d1a2a4440c77c42c205014045ff1ab5417fb1
Muthazhagu (Muthu) Palanisamy <muthazhagu@gmail.com>
2014-08-21 12:44:05 -0400

    Added comments for all methods.

--- filehandling.py
+++ filehandling.py
@@ -6,7 +6,17 @@ import glob
 from ViolationRecordProcessing import create_vr_file
 
 
+"""
+This module has a number of methods that help with file handing.
+
+Essentially, it sets up the directories needed for the tabulatepollutantdata.py program to run.
+"""
+
 def findos():
+    """
+    Method does not return anything.
+    If the OS is neither posix based, nor Windows, the program will quit.
+    """
     if os.name == 'posix':
         root_dir = '/'
     elif os.name == 'nt':
@@ -16,7 +26,22 @@ def findos():
         sys.exit()
 
 
+def checkworkingdir():
+    """
+    Method return True or False, depending on whether the directory from which
+    this program is run called epa (small-case).
+    """
+    # os.chdir('/Users/muthu/Downloads/keerthi/eis_report/epa')  # DO NOT UNCOMMENT THIS LINE
+    workingdir = os.getcwd()  # always run this program from within a directory named epa
+    return workingdir.endswith('epa')
+
+
 def recreatedir(workingdir, dirname):
+    """
+    :param workingdir: workingdir in most cases must be called epa. Every other dir will be a sub-dir of epa.
+    :param dirname: Any of the other dirs needed for this program. dirname is a sub-dir of epa.
+    :return: Program returns nothing.
+    """
     if os.path.exists(os.path.join(workingdir, dirname)):
         shutil.rmtree(os.path.join(workingdir, dirname))
         os.mkdir(dirname)
@@ -24,13 +49,13 @@ def recreatedir(workingdir, dirname):
         os.mkdir(dirname)
 
 
-def checkworkingdir():
-    # os.chdir('/Users/muthu/Downloads/keerthi/eis_report/epa')  # DO NOT UNCOMMENT THIS LINE
-    workingdir = os.getcwd()  # always run this program from within a directory named epa
-    return workingdir.endswith('epa')
-
-
 def dirsetup():
+    """
+    :return: Returns the path to the dir containing the input files, path to the log dir,
+    processed dir, and the output dir.
+
+    If the working directory is not epa, the program will exit.
+    """
     if checkworkingdir():
         workingdir = os.getcwd()  # must return the epa directory
         recreatedir(workingdir, 'log')
@@ -47,9 +72,19 @@ def dirsetup():
 
 
 def getiofiledata():
+    """
+    :return: Method returns a list containing
+    the absolute path to the input file
+    - e.g.: /path/to/input/IL_results_2001.csv,
+
+    Year of the file - e.g. 2001, and
+
+    the absolute path to the output file
+    - e.g.: /path/to/output/IL_results_2001_output.scv
+
+    for every *.csv file in the input directory.
+    """
     inputdir, logdir, processeddir, outputdir = dirsetup()
-    # os.chdir(inputdir)
-    # workingdir = os.getcwd()  # in the input dir now
     filedata = [
         (os.path.join(inputdir, file), os.path.splitext(file)[0][-4:],
          os.path.join(outputdir, os.path.splitext(file)[0] + '_output.csv'))
@@ -62,6 +97,19 @@ def getiofiledata():
 
 
 def joinfiles():
+    """
+    Method does not return anything.
+
+    It combines all the output files into a single combined output file.
+
+    If a state has data for 2001, 2002, and 2003 as separate output files,
+    this method combines all of it, and produces a single file containing data from
+    2001, 2002, and 2003.
+
+    Individual output files are picked up from the output dir.
+    Combined files are written into the combined dir.
+    Combined have a file name like IL_comcbined_output.csv.
+    """
     if checkworkingdir():
         workingdir = os.getcwd()  # must return the epa directory
         recreatedir(workingdir, 'combined')
@@ -95,6 +143,12 @@ def joinfiles():
 
 
 def sortfiles():
+    """
+    Method does not return anything.
+    It does a lexical sort of the contents of each file in the combined dir.
+
+    This method is NOT being called in the tabulatepollutantdata.py file.
+    """
     if checkworkingdir():
         workingdir = os.getcwd()  # must return the epa directory
         combineddir = os.path.join(workingdir, 'combined')
@@ -116,6 +170,12 @@ def sortfiles():
 
 
 def vrfiles():
+    """
+    Method does not return anything.
+
+    For every file in the combined dir, this file creates another file called, for example -
+    followup_details_IL_combined_output.csv
+    """
     if checkworkingdir():
         workingdir = os.getcwd()  # must return the epa directory
         combineddir = os.path.join(workingdir, 'combined')
