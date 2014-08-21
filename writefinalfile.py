68038690aefd2990618facdbb52e35e0eefc07df
Muthazhagu (Muthu) Palanisamy <muthazhagu@gmail.com>
2014-08-21 13:14:10 -0400

    First commit.

--- writefinalfile.py
+++ writefinalfile.py
@@ -3,6 +3,17 @@ from buildrowobjectdict import buildrowobjectdict
 
 
 def writefinalfile(filename, finalresultfile, year):
+    """
+    :param filename: name of the input file
+    :param finalresultfile: name of the output file
+    :param year: year of the data
+    :return: nothing
+
+    This method creates the files in the output directory, for each file in the input directory.
+
+    Method name is a misnomer - it really does not write the final result, but instead a file needed
+    for intermediate data processing.
+    """
     with open(filename) as f:
         finalresultfile.writelines(','.join(
             ['eis_facility_site_id', 'facility_site_name', 'addr_state_cd', 'year', '7439921', 'CO', 'NOX', 'PM10-PRI',
